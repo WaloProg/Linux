46fe8c02e57b805908e8742e7228f8189bd6b979
Wladimir <github@wladimir-kuschnir.de>
2024-09-10 09:41:18 +0200

    Hello

--- git.sh
+++ git.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "You want to commit all changes? [Enter = yes / n = no / q = cancel script]"
 
+read commit_all
 
-echo "Hey"
+echo "Please enter your commit massage."
+
+read commit_massage
+
+if [ -z $read]; then
+    git commit -a -m $commit_massage
+
+fi
