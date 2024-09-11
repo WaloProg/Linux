437c9e53879377056cf7a4c2613f4224e80351c3
Wladimir <github@wladimir-kuschnir.de>
2024-09-11 06:57:57 +0200

    first function

--- git.sh
+++ git.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "You want to commit all changes? [Enter = yes / n = no / q = cancel script]"
-
-read commit_all
+read -p "You want to commit all changes? [y = yes / n = no / q = cancel script]" commit_all
 
 echo "Please enter your commit massage."
 
 read commit_massage
 
-if [ -z $read]; then
-    git commit -a -m $commit_massage
-
+if [ $commit_all = "y" ]; then
+    #git commit -a -m $commit_massage
+    echo $commit_all
+else 
+    echo "Wrong input. The script will be canceled."
 fi
