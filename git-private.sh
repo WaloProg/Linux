8ff0e56e815795bbc6babf612236deda6a8ed41a
Wladimir <github@wladimir-kuschnir.de>
2024-09-16 20:58:50 +0200

    Third Try

--- git-private.sh
+++ git-private.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/expect
+#!/bin/bash
 
 
 
@@ -11,10 +11,12 @@ echo "Please enter your commit massage."
 read -r commit_massage
 
 if [ "$commit_all" = "y" ]; then
-    #git commit -a -m "$commit_massage"
+    git add .
+    git commit -a -m "$commit_massage"
+    git push
     echo "$commit_all"
 elif [ "$commit_all" = "n" ]; then
-    git commit -a -m "$commit_massage
+    git commit -a -m "$commit_massage"
 else 
     echo "Wrong input. The script will be canceled."
 fi
